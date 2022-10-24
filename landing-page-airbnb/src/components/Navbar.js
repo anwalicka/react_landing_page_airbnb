eccda8d449afb550cc9e1835ce4ff00c90f341cc
Anna <you@example.com>
2022-10-25 00:16:36 +0200

    simple navbar styling

--- landing-page-airbnb/src/components/Navbar.js
+++ landing-page-airbnb/src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import airbnb from '../images/airbnb.svg';
 export default function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <div className='nav--logo-container'>
+            <img className='nav--img' src={airbnb}></img>
+            <span className='nav--text'>airbnb</span>
+            </div>
+        </nav>
     );
 }
