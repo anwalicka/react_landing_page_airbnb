5dcb9bdadb3f8751eccd58708555a8674b70d0a7
Anna <you@example.com>
2022-10-29 14:35:38 +0200

    add props to card component

--- landing-page-airbnb/src/App.js
+++ landing-page-airbnb/src/App.js
@@ -3,12 +3,55 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
+import hero5 from '../src/images/hero5.jpg';
+import hero6 from '../src/images/hero6.jpg';
+import hero4 from '../src/images/hero4.jpg';
+import hero3 from '../src/images/hero3.jpg';
 function App() {
+  const cardData = [
+    {img: hero5, description: "Life lessons with Karol Smith Junior"},
+    {img: hero6,description: "Life lessons with Jane"},
+    {img: hero4, description: "Life lessons with Paulo"},
+    {img: hero3, description: "Life lessons with Alik"},
+  ];
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      <div className='cards-container'>
+        <Card
+          img={hero5}
+          description="Life lessons with Karol Smith Junior"
+          rating={6.9}
+          reviewCount={5}
+          country="NL"
+          price="$250"
+          />
+        <Card
+          img={hero6}
+          description="Life lessons with Jane"
+          rating={4.3}
+          reviewCount={10}
+          country="PL"
+          price="$900"
+          />
+        <Card 
+          img={hero4}
+          description="Life lessons with Paulo"
+          reviewCount={2}
+          rating={2.7}
+          country="DE"
+          price="$300"
+        />
+        <Card
+          img={hero3}
+          description="Life lessons with Alik"
+          reviewCount={6}
+          rating={5.0}
+          country="USA"
+          price="$550"
+        />
+      </div>
     </div>
 
     );
--- landing-page-airbnb/src/components/Card.js
+++ landing-page-airbnb/src/components/Card.js
@@ -1,17 +1,22 @@
 import React from "react";
 import hero5 from '../images/hero5.jpg';
 import starSolid from '../images/star-solid.svg'
-export default function Card() {
+export default function Card({img, description, rating, reviewCount, country, price}) {
     return (
-        <section className="section--card">
-            <img className="section--card__img" src={hero5}></img>
+        
+            <section className="section--card">
+            <img className="section--card__img" src={img}></img>
             <div className="section--card--rating-container">
                 <span>
                     <img className="section--card--rating__star-img" src={starSolid} />
                 </span>
-                <span className="section--card-rating__text">5/5(6) USA</span>
+                <span className="section--card-rating__text">{rating}({reviewCount}) {country}</span>
             </div>
-            <p className="section--card__description">Life lessons with Karoline Smith Junior</p>
+            { description && <p className="section--card__description">{description}</p> }
+            <p className="section--card__price">
+                <span className="section--card__price--bold">From {price}</span>
+                <span className="section--card__price--normal">/ person</span>
+            </p>
         </section>
     );
 }
